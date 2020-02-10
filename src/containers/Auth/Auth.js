2ce06ea8f372bdbf568d94c4de0d9f0731d60184
honeybaey <honeybaey@gmail.com>
2020-02-10 21:19:38 +0300

    adding the ability to register and login

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import classes from './Auth.module.css'
 import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
 import is from 'is_js'
+import axios from 'axios'
 
 export default class Auth extends Component {
 
@@ -35,12 +36,36 @@ export default class Auth extends Component {
     }
   }
 
-  loginHandler = () => {
+  loginHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    }
 
+    try {
+      const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCPRT23PJdf04cIwRExpxE93lS5bnMyPEI', authData)
+
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  registerHandler = () => {
-    
+  registerHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    }
+
+    try {
+      const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCPRT23PJdf04cIwRExpxE93lS5bnMyPEI', authData)
+
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   submitHandler = event => {
